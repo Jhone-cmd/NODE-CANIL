2eab0be0d7ae748aaef170d44c3cd419b98451c3
Jhone-cmd <jhone.1941@gmail.com>
2022-10-19 20:38:40 -0300

    removing html folder

--- src/server.ts
+++ src/server.ts
@@ -4,27 +4,20 @@ import mustache from'mustache-express';
 import path from 'path';
 import mainRoutes from './routes/index'
 
-// Variáveis de Ambiente
 dotenv.config();
 
-// Servidor do Sistema
 const server = express();
 
-// Template Engine Usado no HTML
 server.set('view engine', 'mustache');
 server.set('views', path.join(__dirname, 'views'));
 server.engine('mustache', mustache());
 
-// Pasta Pública Estática 
 server.use(express.static(path.join(__dirname, '../public')));
 
-//ROTAS
 server.use(mainRoutes);
 
-// Página Não Encontrada
 server.use((req, res) => {
-    res.status(404).send('página não encontrada')
+    res.render('pages/404');
 });
 
-// Running System on Port 3000
 server.listen(process.env.PORT);
